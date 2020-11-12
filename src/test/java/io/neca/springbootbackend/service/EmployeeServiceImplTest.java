7e0ff55300368257cfd7f39f2e0081cd2c58aefc
necayoka <necayoka@gmail.com>
2020-11-12 13:11:47 +0100

    some tests

---
+++ src/test/java/io/neca/springbootbackend/service/EmployeeServiceImplTest.java
@@ -0,0 +1,26 @@
+package io.neca.springbootbackend.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeeServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    @DisplayName("Should return the employee from db")
+    void getOne() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
