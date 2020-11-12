8d312dd3d1900fba9a443a1cc4cd1d2b56711a22
necayoka <necayoka@gmail.com>
2020-11-12 16:52:09 +0100

    nothing

--- src/test/java/io/neca/springbootbackend/controller/EmployeeControllerTest.java
+++ src/test/java/io/neca/springbootbackend/controller/EmployeeControllerTest.java
@@ -1,6 +1,9 @@
 package io.neca.springbootbackend.controller;
 
 import static org.assertj.core.api.Assertions.*;
+
+import io.neca.springbootbackend.model.Employee;
+import io.neca.springbootbackend.service.EmployeeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.engine.TestExecutionResult;
@@ -14,6 +17,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -24,12 +29,22 @@ class EmployeeControllerTest {
     private EmployeeController controller;
 
     @Autowired
+    EmployeeService service;
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
     @DisplayName("Just simple testing with assertj")
-    public void contextLoads() throws Exception {
+    public void getEmployeeTest() throws Exception {
         this.mockMvc.perform(get("/api/v1/employees")).andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("assertj test version 2 i hope this will work")
+    public void  getEmployeeTestV2() {
+        Optional<Employee> employee = service.getOne(1);
+        assertThat(employee).isNotNull();
     }
 
 }
